e22e97d9d5fa5c50b6f2673db477884182627f30
Reilly Brogan <reilly@reillybrogan.com>
2025-09-01 13:07:07 -0500

    plasma-browser-integration: Adjust per KDE recommendations
    Signed-off-by: Reilly Brogan <reilly@reillybrogan.com>

---
+++ p/plasma-browser-integration/pkg/firefox-prefs.js
@@ -0,0 +1,5 @@
+// This file ships configuration options which enhance integration between Firefox and KDE Plasma
+
+// https://community.kde.org/Distributions/Packaging_Recommendations#Firefox_configuration
+// Disable the hardware media keys as they conflict with the integration provided by the browser extension
+pref("media.hardwaremediakeys.enabled",      false);
